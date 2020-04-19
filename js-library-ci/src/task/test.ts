03f9e8cc1de260879a872e4c9157238cf35a0c72
Richard Ainger <raing3@gmail.com>
2020-04-19 15:58:18 +1000

    Fixed bag test command being used.

--- js-library-ci/src/task/test.ts
+++ js-library-ci/src/task/test.ts
@@ -3,7 +3,7 @@ import * as core from "@actions/core";
 
 export const test = async (packageContent: any) => {
     if (packageContent?.scripts?.test) {
-        await exec.exec('run test --silent');
+        await exec.exec('npm test --silent');
     } else {
         core.info('No test script specified, skipping.');
     }
